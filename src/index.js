d5c40734a7bc956dfaf62e1326a293890af06a0c
arush-hub02 <arushdeepguptacse@gmail.com>
2024-07-15 22:29:15 +0530

    index update

--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
 // require("dotenv").config({path: './env});
 
 import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+
 dotenv.config({
   path: "./env",
 });
 
-import connectDB from "./db/db.js";
-
 connectDB();
-
-/*
-import express from "express";
-const app = express();
-
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.log("error");
-      throw error;
-    });
-
-    app.listen(process.env.PORT, () => {
-      console.log("server is running on port ", process.env.PORT);
-    });
-  } catch (error) {
-    console.error("ERROR: ", error);
-    throw error;
-  }
-})();
-*/
